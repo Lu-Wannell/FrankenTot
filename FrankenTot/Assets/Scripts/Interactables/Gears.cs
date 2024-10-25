cbd14cfe4b09fa5d8f89e4f0065a7dceb90c8272
Lu-Wannell <2727686@students.wits.ac.za>
2024-10-25 17:25:09 +0200

    added code

--- FrankenTot/Assets/Scripts/Interactables/Gears.cs
+++ FrankenTot/Assets/Scripts/Interactables/Gears.cs
@@ -11,6 +11,11 @@ public class Gears : Interactable
     [SerializeField]
     private JuxeBoxController jukeBoxController;
 
+    [Header("Panel Info")]
+    [Space(7)]
+    [SerializeField]
+    private bool isPanelOpen;
+
 
     [Header("Gear Info")]
     [Space(7)]
@@ -18,27 +23,70 @@ public class Gears : Interactable
     private bool hasGearOne = false;
     [SerializeField]
     private bool hasGearTwo = false;
+    [SerializeField]
+    private bool hasGearThree = false;  
 
 
     [SerializeField]
-    private GameObject GearOne;
+    private GameObject gearOne;
     [SerializeField]
-    private GameObject GearTwo;
+    private GameObject gearTwo;
+    [SerializeField]
+    private GameObject gearThree;
 
 
     public Transform gearTargetOne;
     public Transform gearTargetTwo;
+    public Transform gearTargetThree;
 
 
     protected override void Interact()
     {
-        hasGearOne = true;
-        hasGearTwo = true;
-        if (hasGearOne && hasGearTwo)
+        if (jukeBoxController.hasAllGears == false)
+        {
+            if (firstPersonControls.heldObject != null)
+            {
+
+                if(firstPersonControls.heldObject == gearOne)
+                {
+                    hasGearOne = true;
+
+                    PlaceGear(gearTargetOne);
+                }
+                else if(firstPersonControls.heldObject == gearTwo)
+                {
+                    hasGearTwo = true;
+                    PlaceGear(gearTargetTwo);
+                }
+                else if(firstPersonControls.heldObject == gearThree)
+                {
+                    hasGearThree = true;
+                    PlaceGear(gearTargetThree);
+                }
+                else
+                {
+                
+                }
+
+            }
+        }
+        
+        if (hasGearOne && hasGearTwo && hasGearThree)
         {
             jukeBoxController.hasAllGears = true;
         }
         
     }
+
+
+    private void PlaceGear(Transform gearTarget)
+    {
+        firstPersonControls.heldObject.GetComponent<Rigidbody>().isKinematic = true; //disable physics
+                                                                                     // Attach the object to the target position
+        firstPersonControls.heldObject.transform.position = gearTarget.position;
+        firstPersonControls.heldObject.transform.rotation = (gearTarget.rotation);
+        firstPersonControls.heldObject.transform.parent = gearTarget;
+    }
+
 }
 
